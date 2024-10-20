13431bd281ebec071e4d9785dc228c2a96912f08
chrstnlai <christinelai04@gmail.com>
2024-10-19 21:37:27 -0700

    added block component

--- sundial/src/app/page.js
+++ sundial/src/app/page.js
@@ -1,14 +1,15 @@
 import DynamicIsland from "./conponents/chat/ChatMain";
 import MainHeader from "./conponents/header/MainBar";
+import Block from "./conponents/block/Block";
 
 export default function Home() {
   return (
-    <div className="w-screen h-screen flex flex-col items-center justify-center ">
+    <div className="w-screen h-screen flex flex-col items-center justify-center">
       <div className="flex flex-col h-full max-w-[800px] max-h-[600px] shadow rounded-xl">
         <MainHeader />
 
-        <div className="flex-grow">{/* Main content goes here */}</div>
-
+        <div className="flex flex-col items-center flex-grow justify-center"> </div>
+        <Block />
         <DynamicIsland className="h-14" />
       </div>
     </div>
